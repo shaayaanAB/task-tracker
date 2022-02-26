358e73d3e0956d52b5133430ae117d1178424fa9
ShayanAB+ <a.b.shayan@protonmail.com>
2022-02-26 07:35:37 +0330

    My react project, task manager with json builtin data base :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+// import About from './components/About';
+
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect( () => {
+      const getTasks = async() => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //fetch tasks
+  const fetchTasks = async() => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+    //fetch task
+    const fetchTask = async(id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      return data
+    }
+
+ const noTaskAvailable = () => {
+  // show a animated spinner when no task available 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div className='spinner'>
+       <div className='dot'></div>
+       <div className='dot'></div>
+       <div className='dot'></div>
+       <div className='dot'></div>
+       <div className='dot'></div>
+     </div>
+   )
+ }
+ 
+  // add new task
+ const addTask = async (task) => {
+   const res = await fetch('http://localhost:5000/tasks',
+   {
+     method: 'POST', 
+     headers: {
+      'Content-type' : 'application/json'
+      },
+      body:
+        JSON.stringify(task),
+    })
+
+  const data = await res.json()
+  setTasks([...tasks, data])
 }
 
+  // delete a task
+  const deleteTask = async (id) => {
+    fetch(`http://localhost:5000/tasks/${id}`,
+     {method: 'DELETE'}
+    )
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
+  // toggle task's reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle,
+       reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = await res.json()
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task ))
+  }
+
+  return (
+    <Router>
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        { tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : noTaskAvailable() }
+        {/* <Route path='/about' component={About} /> */}
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+  
 export default App;
